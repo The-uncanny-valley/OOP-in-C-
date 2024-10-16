b5831a6b444bad1fdf7f68ea6570bfb7417b355a
The-uncanny-valley <118472622+Workalina@users.noreply.github.com>
2024-10-16 23:14:05 +0300

    task 4 added

---
+++ unit-2-task-4.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+
+/*“Dreams” by Langston Hughes*/
+
+int main() {
+    cout << "Hold fast to dreams" << "\n" << "For if dreams die" << "\n" << "Life is a broken-winged bird" << "\n" << "That cannot fly." <<
+    "\n" << "Hold fast to dreams" << "\n" << "For when dreams go" << "\n" << "Life is a barren field" << "\n" << "Frozen with snow." << endl;
+}
