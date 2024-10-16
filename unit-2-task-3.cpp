f82d69812241d69c874f15c4e86faf50b5875190
The-uncanny-valley <118472622+Workalina@users.noreply.github.com>
2024-10-16 22:02:35 +0300

    task 3 added

---
+++ unit-2-task-3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int var1 = 10; // целая константа
+    cout << var1 << endl;
+    var1 *= 2;
+    cout << var1 << endl << --var1 << endl;
+}
