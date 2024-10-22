3526117f33ebb15858d07e1785e698bef78dca66
The-uncanny-valley <118472622+Workalina@users.noreply.github.com>
2024-10-22 21:56:59 +0300

    task 12 added

---
+++ unit-2-task-12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int pounds;
+    float decimalPenny;
+    int shilling, penny;
+    cin >> pounds >> decimalPenny;
+    shilling = static_cast<int>(decimalPenny * 20);
+    penny = static_cast<int>(((decimalPenny * 20) - shilling) * 12);
+    cout << pounds << "." << shilling << "." << penny << endl;
+    return 0;
+}
